7676e7cd143cb5c73e32ed1bd79659980a4e6c89
Vinícius Alves <viniciusasetorio@gmail.com>
2024-06-27 15:37:40 -0300

    Beecrowd - Exercício 13

---
+++ Exercicio13/Main.java
@@ -0,0 +1,37 @@
+/*
+Escreva um programa que leia três valores com ponto flutuante de dupla precisão: A, B e C. Em seguida, calcule e mostre:
+a) a área do triângulo retângulo que tem A por base e C por altura.
+b) a área do círculo de raio C. (pi = 3.14159)
+c) a área do trapézio que tem A e B por bases e C por altura.
+d) a área do quadrado que tem lado B.
+e) a área do retângulo que tem lados A e B.
+Entrada
+O arquivo de entrada contém três valores com um dígito após o ponto decimal.
+
+Saída
+O arquivo de saída deverá conter 5 linhas de dados. Cada linha corresponde a uma das áreas descritas acima, sempre com mensagem correspondente e um espaço entre os dois pontos e o valor. O valor calculado deve ser apresentado com 3 dígitos após o ponto decimal.
+ */
+
+package Exercicio13;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        double A, B, C;
+        A = scanner.nextDouble();
+        B = scanner.nextDouble();
+        C = scanner.nextDouble();
+
+        double areaTrianguloRetangulo = (A * C) / 2;
+        double areaCirculo = 3.14159 * Math.pow(C, 2);
+        double areaTrapezio = ((A + B) * C) / 2;
+        double areaQuadrado = Math.pow(B, 2);
+        double areaRetangulo = A * B;
+
+        System.out.printf("TRIANGULO: %.3f\nCIRCULO: %.3f\nTRAPEZIO: %.3f\nQUADRADO: %.3f\nRETANGULO: %.3f\n",
+                areaTrianguloRetangulo, areaCirculo, areaTrapezio, areaQuadrado, areaRetangulo);
+    }
+}
